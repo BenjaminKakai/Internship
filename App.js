dc8a72a9cc1242d0ab97029da7ee73f2f73e13f2
benjamin kakai <benjaminkakai001@gmail.com>
2023-11-13 15:33:02 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,83 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
-
-import Header from './Header/Header';
+import React from 'react';
+import NavbarTop from './NavbarTop/NavbarTop';
+import NavbarBottom from './NavbarBottom/NavbarBottom';
+import ShowcaseContent from './ShowcaseContent/ShowcaseContent';
 import Services from './Services/Services';
 import Goal from './Goal/Goal';
 import Portfolio from './Portfolio/Portfolio';
 import Teams from './Teams/Teams';
-import Blog from './Blog /Blog';  // Note: Ensure the space is intentional after "Blog"
+import Blog from './Blog/Blog';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
-import Home from './Home/Home';
 
-import NavyBlueStrip from './NavyBlueStrip/NavyBlueStrip';  // New component import
-import WhiteStripWithAnimation from './WhiteStripWithAnimation/WhiteStripWithAnimation';  // New component import
-
-import NavigationBar from './NavigationBar/NavigationBar';
-
-import './index.css';
-
-function AppContent() {
-  const [isNavbarSticky, setNavbarSticky] = useState(false);
-  const [showScrollUpBtn, setShowScrollUpBtn] = useState(false);
-  const [isContactVisible, setContactVisible] = useState(false);
-  
-  const location = useLocation();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setNavbarSticky(window.scrollY > 5);
-      setShowScrollUpBtn(window.scrollY > 500);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
-  const handleGoBack = () => {
-    setContactVisible(false);
-  };
-
+function App() {
   return (
     <div>
-      <Header sticky={isNavbarSticky} onContactClick={() => setContactVisible(true)} />
-      
-      {isContactVisible ? (
-        <Contact isVisible={isContactVisible} onGoBack={handleGoBack} />
-      ) : (
-        <>
-          <Home />
-          <NavyBlueStrip />
-          <WhiteStripWithAnimation />
-          <Services />
-          <Goal />
-          <Portfolio />
-          <Teams />
-          <Blog />
-          <Footer />
-        </>
-      )}
-      
-      {showScrollUpBtn && <button className="scroll-up-btn" onClick={scrollToTop}>Scroll Up</button>}
+      <NavbarTop />
+      <NavbarBottom />
+      <ShowcaseContent />
+      <Services />
+      <Goal />
+      <Portfolio />
+      <Teams />
+      <Blog />
+      <Contact />
+      <Footer />
     </div>
   );
 }
 
-function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-}
-
 export default App;
-
